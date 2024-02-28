1318b7f5e9aac5e6694266e6c548f98a704bef70
Gabriela Paulino <gabrielappsilva18@gmail.com>
2024-02-28 19:13:08 -0300

    Adição de script padrão para as aulas de DQL

---
+++ aulas/09/GESTAOACADEMIA.sql
@@ -0,0 +1,11 @@
+FROM Aluno
+WHERE Matricula < 10
+ORDER BY Nome;
+
+SELECT  * FROM ALUNO;
+
+SELECT A.Nome FROM ALUNO A;
+
+SELECT PF.Professor_ID FROM professores_disciplinas PF;
+
+SELECT Nome AS nm FROM Aluno;
