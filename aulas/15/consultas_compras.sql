15097f23a617a5efd5d603ea5ccf104c5b9bd46f
Gabriela Paulino <gabrielappsilva18@gmail.com>
2024-03-12 19:35:10 -0300

    Finalização de consultas básicas de compras

--- aulas/15/consultas_compras.sql
+++ aulas/15/consultas_compras.sql
@@ -37,3 +37,37 @@ SELECT
     JOIN itenspedido ON pedido.id = itenspedido.pedidoId
     JOIN produto ON itenspedido.produtoId = produto.id
     GROUP BY pedido.clienteId;
+    
+-- Encontrar produtos com o preço entre 50 e 150 reais
+SELECT * 
+FROM produto
+WHERE preco >= 50 AND preco <= 150;
+
+SELECT * FROM produto
+WHERE preco BETWEEN 50 AND 150;
+
+-- Mostre tudo dos produtos com ID 1, 5 e 7
+SELECT * FROM produto
+WHERE id = 1 OR id = 5 OR id = 7;
+
+SELECT * FROM produto
+WHERE id IN (1, 3, 7);
+
+-- Mostre todos os clientes que o sobremnome termine com "a"
+SELECT * FROM cliente
+WHERE sobrenome LIKE '%a';
+
+-- Mostre os 5 produtos mais caros da loja
+SELECT * FROM produto
+ORDER BY preco DESC
+LIMIT 5;
+
+-- Listando os produtos por página, cada página cabendo 2 produtos, me mostre a página 3
+SELECT * FROM produto
+LIMIT 2 OFFSET 4;
+
+-- Consultar os pedidos feitos entre 2024-03-01 e 2024-03-05, limitando a 5 resultados
+SELECT * FROM pedido
+WHERE dataPedido BETWEEN '2024-03-01' AND '2024-03-05'
+ORDER BY dataPedido
+LIMIT 5;
